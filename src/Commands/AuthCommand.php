56c1cff3b1a83e95f3632127d34dbb34930ae276
Vic Rubba <crazedVic@users.noreply.github.com>
2021-02-05 23:11:05 -0700

    Update AuthCommand.php

--- src/Commands/AuthCommand.php
+++ src/Commands/AuthCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Redbastie\Skele\Commands;
+namespace Crazed\Crudwired\Commands;
 
 use Illuminate\Console\Command;
 use Livewire\Commands\ComponentParser;
@@ -10,25 +10,25 @@ class AuthCommand extends Command
 {
     use ManagesFiles;
 
-    protected $signature = 'skele:auth {--force}';
+    protected $signature = 'crudwired:auth {--force}';
 
     public function handle()
     {
 
-        $factoryParser = new ComponentParser(config('skele.factory_path'),  config('livewire.view_path'), 'UserFactory');
+        $factoryParser = new ComponentParser(config('crudwired.factory_path'),  config('livewire.view_path'), 'UserFactory');
 
         $this->createFiles('auth', [
             'components'  =>config('livewire.class_namespace'),
             'views' => config('livewire.view_path'),
-            'models' => config('skele.model_path'),
-            'factories' => base_path() .DIRECTORY_SEPARATOR . config('skele.factory_path'),
-            'DummyModelNamespace' => config('skele.model_path'),
+            'models' => config('crudwired.model_path'),
+            'factories' => base_path() .DIRECTORY_SEPARATOR . config('crudwired.factory_path'),
+            'DummyModelNamespace' => config('crudwired.model_path'),
             "DummyComponentNamespace" => config('livewire.class_namespace'),
             'DummyFactoryNamespace' => $factoryParser->classNamespace()
 
         ], $this->option('force'));
 
-        $command_string = 'skele:migrate';
+        $command_string = 'crudwired:migrate';
         $this->info("Calling: " . $command_string);
         Artisan::call($command_string, [], $this->getOutput());
 
