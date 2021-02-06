cbce3a43c1aee1fbac2f168a47965e40159c5d0a
Victor Rubba <vrubba@gmail.com>
2021-02-06 09:51:19 -0700

    cross platform path bug fixed

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,21 @@ use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')->group(function () {
-
+    
     $filesystem = new Filesystem;
-    $dir = base_path() . DIRECTORY_SEPARATOR .  config('livewire.class_namespace');
+    //$dir = base_path() . DIRECTORY_SEPARATOR .  config('livewire.class_namespace');
+
+    $component_relative_path = str_replace(['\\', 'App'],
+            [DIRECTORY_SEPARATOR, 'app'], config('livewire.class_namespace'));
+
+    $dir = base_path() . DIRECTORY_SEPARATOR . $component_relative_path;
 
     if ($filesystem->exists($dir)) {
         foreach ($filesystem->allFiles($dir) as $file) {
+
             $namespace = config('livewire.class_namespace') . "\\" . 
                 str_replace(['/', '.php'], ['\\', ''], $file->getRelativePathname());
+
             $class = app($namespace);         
 
             if (property_exists($class, 'routeUri') && $class->routeUri) {
@@ -21,8 +28,7 @@ Route::middleware('web')->group(function () {
                 if (property_exists($class, 'routeName') && $class->routeName) {
                     $route->name($class->routeName);
                     //error_log($class->routeName);
-                   
-                }
+              }
 
                 if (property_exists($class, 'routeMiddleware') && $class->routeMiddleware) {
                     $route->middleware($class->routeMiddleware);
