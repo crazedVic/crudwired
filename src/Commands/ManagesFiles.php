2587238cb7ff2e6b3b52e2c5f9bd10d34e04fe9f
Vic Rubba <crazedVic@users.noreply.github.com>
2021-02-05 23:18:12 -0700

    Update ManagesFiles.php

--- src/Commands/ManagesFiles.php
+++ src/Commands/ManagesFiles.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Redbastie\Skele\Commands;
+namespace Crazed\Crudwired\Commands;
 
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Str;
@@ -24,9 +24,11 @@ trait ManagesFiles
         foreach ($this->filesystem()->allFiles($working_folder) as $file) {
             $this->info('processing file: ' . $file);
             $filePath = Str::replaceLast('.stub', '', $file->getRelativePathname());
+            
             // fix for multi-OS support
             $filePath = str_replace('/', DIRECTORY_SEPARATOR, $filePath);
             $this->warn("Will attempt to modify file:  " . $filePath);
+            
             if ($this->filesystem()->exists($filePath)) {
                 $existing_file = $this->filesystem()->get($filePath);
                 if( !str_contains($existing_file, trim($file->getContents()))){
@@ -59,9 +61,6 @@ trait ManagesFiles
             if ($fileDir = implode(DIRECTORY_SEPARATOR, array_slice(explode(DIRECTORY_SEPARATOR, $filePath), 0, -1))) {
                 $this->filesystem()->ensureDirectoryExists($fileDir);
             }
-
-           // $this->filesystem()->put($filePath, $this->replace($replaces, $file->getContents()));
-           // $this->warn('Created file: <info>' . $filePath . '</info>');
 
             // prevent override by default
             if ($force || !$this->filesystem()->exists($filePath)) {
@@ -96,30 +95,5 @@ trait ManagesFiles
         }
 
         return $contents;
-    }
-
-    private function mapPath($originalPath)
-    {
-        $arr = explode(DIRECTORY_SEPARATOR, $originalPath, 2);
-        $category = $arr[0];
-        $filename = $arr[1];
-
-        switch ($category){
-            case 'views':
-                return config('livewire.view_path') . DIRECTORY_SEPARATOR . $filename;
-                break;
-            case "models":
-                return config('skele.model_path') . DIRECTORY_SEPARATOR . $filename;
-                break;
-            case "components":
-                return config('livewire.class_namespace') . DIRECTORY_SEPARATOR . $filename;
-                break;
-            case "factories":
-                return config('skele.factory_path') . DIRECTORY_SEPARATOR . $filename;
-                break;
-            default:
-                return $originalPath;
-
-        }
     }
 }
